952180a02ca1d1f98d0632c84f0623fbceb39855
nardis556 <82609175+nardis556@users.noreply.github.com>
2023-05-10 00:10:28 -0500

    Create check_scripts.sh

---
+++ check_scripts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if pgrep -f "build_followers.py" > /dev/null
+then
+    echo "build_followers.py is running."
+else
+    echo "build_followers.py is not running. Starting build_followers.py"
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/build_followers.py &
+fi
+
+if pgrep -f "new_followers.py" > /dev/null
+then
+    echo "new_followers.py is running."
+else
+    echo "new_followers.py is not running. Starting new_followers.py"
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/new_followers.py &
+fi
+
+if /home/$USER/.pyenv/shims/python -f "tweet_metrics.py" > /dev/null
+then
+    echo "tweet_metrics.py is running."
+else
+    echo "tweet_metrics.py is not running. Starting tweet_metrics.py"
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/tweet_metrics.py &
+fi
+
+if pgrep -f "update_summaries.py" > /dev/null
+then
+    echo "update_summaries.py is running."
+else
+    echo "update_summaries.py is not running. Starting update_summaries.py"
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/update_summaries.py &
+fi
