6c26a38dc59f82e6b13d6bd416f5ed36234208e5
nardis556 <82609175+nardis556@users.noreply.github.com>
2023-05-11 11:07:38 -0500

    Update check_scripts.sh

--- cron/check_scripts.sh
+++ cron/check_scripts.sh
@@ -5,7 +5,7 @@ then
     echo "build_followers.py is running."
 else
     echo "build_followers.py is not running. Starting build_followers.py"
-    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/build_followers.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/src/build_followers.py &
 fi
 
 # if pgrep -f "new_followers.py" > /dev/null
@@ -13,7 +13,7 @@ fi
 #     echo "new_followers.py is running."
 # else
 #     echo "new_followers.py is not running. Starting new_followers.py"
-#     /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/new_followers.py &
+#     /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/src/new_followers.py &
 # fi
 
 if /home/$USER/.pyenv/shims/python -f "tweet_metrics.py" > /dev/null
@@ -21,7 +21,7 @@ then
     echo "tweet_metrics.py is running."
 else
     echo "tweet_metrics.py is not running. Starting tweet_metrics.py"
-    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/tweet_metrics.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/src/tweet_metrics.py &
 fi
 
 if pgrep -f "update_summaries.py" > /dev/null
@@ -29,5 +29,5 @@ then
     echo "update_summaries.py is running."
 else
     echo "update_summaries.py is not running. Starting update_summaries.py"
-    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/update_summaries.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/src/update_summaries.py &
 fi
