829dc2049fe4b01a1d3d14b997bf4dbbff93d6f5
nardis556 <82609175+nardis556@users.noreply.github.com>
2023-05-11 11:28:50 -0500

    Update author_engagements.sql

--- build_tables/author_engagements.sql
+++ build_tables/author_engagements.sql
@@ -1,17 +1,17 @@
-CREATE TABLE author_daily_engagements (
-    date DATE NOT NULL,
+CREATE TABLE author_daily_engagements_summaries (
+    date DATE,
     engagements INT,
     PRIMARY KEY (date)
 );
 
-CREATE TABLE author_weekly_engagements (
-    date DATE NOT NULL,
+CREATE TABLE author_weekly_engagements_summaries (
+    date DATE,
     engagements INT,
     PRIMARY KEY (date)
 );
 
-CREATE TABLE author_monthly_engagements (
-    date DATE NOT NULL,
+CREATE TABLE author_monthly_engagements_summaries (
+    date DATE,
     engagements INT,
     PRIMARY KEY (date)
 );
