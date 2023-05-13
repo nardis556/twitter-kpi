b1029402013193a7775499b2d642f1fe70695dab
nardis556 <user@email>
2023-05-13 19:55:22 +0000

    asdasdsadasdsadas

--- cron/check_scripts.sh
+++ cron/check_scripts.sh
@@ -7,7 +7,7 @@ then
     echo "build_followers.py is running."
 else
     echo "build_followers.py is not running. Starting build_followers.py"
-    /home/lars/.pyenv/shims/python /home/lars/twitter-kpi/scripts/build_followers.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/scripts/build_followers.py &
 fi
 
 # if pgrep -f "new_followers.py" > /dev/null
@@ -15,7 +15,7 @@ fi
 #     echo "new_followers.py is running."
 # else
 #     echo "new_followers.py is not running. Starting new_followers.py"
-#     /home/lars/.pyenv/shims/python /home/lars/twitter-kpi/scripts/new_followers.py &
+#     /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/scripts/new_followers.py &
 # fi
 
 if pgrep -f "tweet_metrics.py" > /dev/null
@@ -23,7 +23,7 @@ then
     echo "tweet_metrics.py is running."
 else
     echo "tweet_metrics.py is not running. Starting tweet_metrics.py"
-    /home/lars/.pyenv/shims/python /home/lars/twitter-kpi/scripts/tweet_metrics.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/scripts/tweet_metrics.py &
 fi
 
 if pgrep -f "update_summaries.py" > /dev/null
@@ -31,7 +31,7 @@ then
     echo "update_summaries.py is running."
 else
     echo "update_summaries.py is not running. Starting update_summaries.py"
-    /home/lars/.pyenv/shims/python /home/lars/twitter-kpi/scripts/update_summaries.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/scripts/update_summaries.py &
 fi
 
 if pgrep -f "author_engagements.py" > /dev/null
@@ -39,7 +39,7 @@ then
     echo "author_engagements.py is running."
 else
     echo "author_engagements.py is not running. Starting author_engagements.py"
-    /home/lars/.pyenv/shims/python /home/lars/twitter-kpi/scripts/author_engagements.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/scripts/author_engagements.py &
 fi
 
 if pgrep -f "user_engagements.py" > /dev/null
@@ -47,7 +47,7 @@ then
     echo "user_engagements.py is running."
 else
     echo "user_engagements.py is not running. Starting user_engagements.py"
-    /home/lars/.pyenv/shims/python /home/lars/twitter-kpi/scripts/user_engagements.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/scripts/user_engagements.py &
 fi
 
 if pgrep -f "author_stats.py" > /dev/null
@@ -55,6 +55,6 @@ then
     echo "author_stats.py is running."
 else
     echo "author_stats.py is not running. Starting author_stats.py"
-    /home/lars/.pyenv/shims/python /home/lars/twitter-kpi/scripts/author_stats.py &
+    /home/$USER/.pyenv/shims/python /home/$USER/twitter-kpi/scripts/author_stats.py &
 fi
 
